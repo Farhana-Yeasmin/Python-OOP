81016be075044907e70478ce1d1ca2e43fcd276f
Farhana-Yeasmin <farhana.cse42@gmail.com>
2024-10-22 22:14:18 +0600

    Inheritance with super function

--- inheritance.py
+++ inheritance.py
@@ -38,3 +38,28 @@ class Son(Father):
         self.vehicle="BMW"
 s= Son()
 # print(s.__dict__)
+
+
+"""
+            Super Function
+- Using super() function, we can access parent class properties
+- Here both parent & child have constructor so child constructor will be the priority and execute
+But I need the parent class constructor also execute. So I will use super()
+"""
+class Computer(object):
+    def __init__(self,ram,storage):
+        # self.ram = '8gb'
+        # self.storage='512gb'
+        self.ram = ram
+        self.storage = storage
+        print('Computer class constructor called')
+
+class Mobile(Computer):
+    def __init__(self,ram,storage):
+        # Here Parent class constructor will execute first
+        super().__init__(ram,storage)
+        self.model ='iphone X'
+        print("Mobile class constructor called")
+
+Apple = Mobile('8gb','512gb')
+print(Apple.__dict__)
--- method.py
+++ method.py
@@ -15,27 +15,3 @@ class Bank:
 # prin=float(input("Enter principle amount:"))
 # n=int(input("Enter number of years:"))
 # Bank.simple_interest(prin,n)
-
-"""
-            Super Function
-- Using super() function, we can access parent class properties
-- Here both parent & child have constructor so child constructor will be the priority and execute
-But I need the parent class constructor also execute. So I will use super()
-"""
-class Computer(object):
-    def __init__(self,ram,storage):
-        # self.ram = '8gb'
-        # self.storage='512gb'
-        self.ram = ram
-        self.storage = storage
-        print('Computer class constructor called')
-
-class Mobile(Computer):
-    def __init__(self,ram,storage):
-        # Here Parent class constructor will execute first
-        super().__init__(ram,storage)
-        self.model ='iphone X'
-        print("Mobile class constructor called")
-
-Apple = Mobile('8gb','512gb')
-print(Apple.__dict__)
